5bc3cdabdb4ea1641f15616d28aa3d1b0f24c4a6
suren <surenspost@gmail.com>
2012-04-26 18:39:47 +0530

    Parsing only mailgun psd

--- app/tasks/utils.rb
+++ app/tasks/utils.rb
@@ -1,6 +1,6 @@
 class Utils
   def self.process_test_file
-    fptr = File.new "/tmp/Dashboard.psd.json"
+    fptr = File.new "/tmp/mailgun.psd.json"
     json_data = fptr.read
     Analyzer.analyze json_data
   end
