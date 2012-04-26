1860568f8ce4020581b1ed0e6fb8cebe63809e4e
suren <surenspost@gmail.com>
2012-04-26 18:40:09 +0530

    Inspect method for layer bounds

--- app/models/photoshop_item/layer_bounds.rb
+++ app/models/photoshop_item/layer_bounds.rb
@@ -6,6 +6,16 @@ class PhotoshopItem::LayerBounds
     @bottom = value[:bottom][:value]
     @left   = value[:left][:value]
     @right  = value[:left][:value]    
+  def inspect
+    s = <<LAYERBOUNDS
+    Layer : #{@parent}
+    Start : #{@top}, #{@left}
+    Width : #{self.width}
+    Height: #{self.height}
+LAYERBOUNDS
+    
+    s
+  end
   end
   
   def get_height
