d0beed44896d8b8384d794ce5125bcb183bd2715
suren <surenspost@gmail.com>
2012-04-27 19:37:57 +0530

    Render html for root node alone

--- app/models/photoshop_item/layer.rb
+++ app/models/photoshop_item/layer.rb
@@ -51,15 +51,24 @@ LAYER
   
   def render_to_html(dom_map, root = false)
     puts "Generating html for #{self.name}"
+    html = ""
     if self.layer[:layerKind] == "LayerKind.TEXT"
       #puts "Text layer"
     elsif self.layer[:layerKind] == "LayerKind.SMARTOBJECT"
       #puts "smart object layer"
     elsif self.layer[:layerKind] == "LayerKind.SOLIDFILL"
-      element = :div
-      element = :body if root
       css = Converter::parse_box self.layer
-      pp css
+      element = :div
+      if root 
+        element = :body
+        css.delete :width
+        css.delete :height
+        css.delete :'min-height'
+      end
+    
+    
+      style_string = Converter::to_style_string css
+      html = content_tag element, nil, :style => style_string
     end
     
     @children.each do |child_index|
@@ -67,6 +76,6 @@ LAYER
       child.render_to_html dom_map
     end
     
-    return ""
+    return html
   end
 end
