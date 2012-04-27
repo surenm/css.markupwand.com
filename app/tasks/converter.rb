5584f6b4f9b9283303b81eea24ffef2b3d8e8d87
suren <surenspost@gmail.com>
2012-04-27 19:19:21 +0530

    Modularize converters and symbolize keys in the JSON file
    Without the module I couldn't access it outside this file

--- app/tasks/converter.rb
+++ app/tasks/converter.rb
@@ -4,10 +4,52 @@ require 'rubygems'
 require 'json'
 require 'pp'
 
-FONT_WEIGHT = {
-  'Regular' => nil,
-  'Bold'    => 'bold'
-}
+module Converter
+  Converter::FONT_WEIGHT = {
+    'Regular' => nil,
+    'Bold'    => 'bold'
+  }
+  
+  def Converter::parse_color(color_object)
+    red   = Integer (color_object[:value][:red][:value])
+    green = Integer (color_object[:value][:grain][:value])
+    blue  = Integer (color_object[:value][:blue][:value])
+
+    '#' + red.to_s(16) + green.to_s(16) + blue.to_s(16)
+  end
+  
+  def Converter::parse_text(layer)
+    #choose first one right now
+    text_style = layer[:textKey][:value][:textStyleRange][:value].first
+
+    css               = {}
+    css[:font-family] = text_style[:value][:textStyle][:value][:fontName][:value]
+    css[:font-size]   = text_style[:value][:textStyle][:value][:size][:value].to_s + 'pt'
+    font_weight       = text_style[:value][:textStyle][:value][:fontStyleName][:value]
+
+    if not FONT_WEIGHT[font_weight].nil?
+      css[:font-weight] = FONT_WEIGHT[font_weight]
+    end
+
+    css[:color] = parse_color(text_style[:value][:textStyle][:value][:color])
+    css
+  end
+
+  def Converter::parse_box(layer)
+    puts layer[:name][:value]
+
+    css                = {}
+    bounds             = layer[:bounds][:value]
+    css[:width]        = (bounds[:right][:value] - bounds[:left][:value]).to_s + 'px'
+    css[:'min-height'] = (bounds[:bottom][:value] - bounds[:top][:value]).to_s + 'px'
+
+    if layer.has_key? :adjustment
+      css[:background]   = parse_color(layer[:adjustment][:value].first[:value][:color])
+    end
+
+    css
+  end
+end
 
 def read_file
   if ARGV.length < 1
@@ -15,55 +57,21 @@ def read_file
   else
     filename = ARGV.first
     data = (File.open(filename)).read
-    return (JSON.parse data)
+    return (JSON.parse data, :symbolize_names => true)
   end
-end
-
-def parse_color(color_object)
-  red   = Integer (color_object['value']['red']['value'])
-  green = Integer (color_object['value']['grain']['value'])
-  blue  = Integer (color_object['value']['blue']['value'])
-  
-  '#' + red.to_s(16) + green.to_s(16) + blue.to_s(16)
-end
-
-def parse_text(text_item)
-  #choose first one right now
-  text_style = text_item['textKey']['value']['textStyleRange']['value'].first
-  css        = {}
-  css['font-family'] = text_style['value']['textStyle']['value']['fontName']['value']
-  css['font-size']   = text_style['value']['textStyle']['value']['size']['value'].to_s + 'pt'
-  font_weight        = text_style['value']['textStyle']['value']['fontStyleName']['value']
-  if not FONT_WEIGHT[font_weight].nil?
-    css['font-weight'] = FONT_WEIGHT[font_weight]
-  end
-  css['color'] = parse_color(text_style['value']['textStyle']['value']['color'])
-  
-  css
-end
-
-def parse_box(box_item)
-  puts box_item['name']['value']
-  css = {}
-  bounds = box_item['bounds']['value']
-  css['width'] = (bounds['right']['value'] - bounds['left']['value']).to_s + 'px'
-  css['min-height'] = (bounds['bottom']['value'] - bounds['top']['value']).to_s + 'px'
-  css['background-color'] = parse_color(box_item['adjustment']['value'].first['value']['color'])
-  
-  css
 end
 
 def parse_file(json)
   json.each do |item|
     css = {}
     if item.has_key? 'textKey'
-      puts "Text item: " +  item['name']['value']
-      css = parse_text(item)
+      puts "Text item: " +  item[:name][:value]
+      css = Converter::parse_text(item)
     elsif item.has_key? 'smartObject'
-      puts "Smart Object: " + item['name']['value']
+      puts "Smart Object: " + item[:name][:value]
     else
-      puts "Box item: " + item['name']['value']
-      css = parse_box(item)
+      puts "Box item: " + item[:name][:value]
+      css = Converter::parse_box(item)
     end
     
     pp css
