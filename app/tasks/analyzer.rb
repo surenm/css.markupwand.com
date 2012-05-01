d9d927f105a1830c459c5b6ca52468a8d92a2607
suren <suren@goyaka.com>
2012-05-02 01:08:26 +0530

    Just void return

--- app/tasks/analyzer.rb
+++ app/tasks/analyzer.rb
@@ -8,13 +8,14 @@ class Analyzer
   
   def self.analyze(psd_json_data)
     psd_layers = JSON.parse psd_json_data, :symbolize_names => true
-
+    
     dom = PhotoshopItem::Dom.create_dom_from_psd psd_layers
-      
+        
     html = self.generate_html dom
     html_fptr = File.new '/tmp/result.html', 'w+'
     html_fptr.write html
     html_fptr.close
-    return true
+    
+    return
   end
 end
