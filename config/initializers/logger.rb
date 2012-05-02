75f19bd7eed873450dbd20943fe7f30f91d57dff
suren <suren@goyaka.com>
2012-05-02 12:59:15 +0530

    Log to Stdout as well

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -3,7 +3,8 @@ include Log4r
 
 module Log
   LOGGER = Log4r::Logger.new 'logger'
-  LOGGER.outputters = Log4r::FileOutputter.new 'debugger', :filename => File.join(Rails.root, "log", "#{Rails.env}-debug.log")
+  LOGGER.outputters = Log4r::FileOutputter.new 'debugger', :filename => File.join(Rails.root, "log", "#{Rails.env}-debug.log")  
+  LOGGER.add Log4r::Outputter.stdout
   
   def Log.method_missing(method, *args, &block)
     LOGGER.send method, args[0]
@@ -13,5 +14,4 @@ module Log
   def Log.level(level)
     LOGGER.level = level
   end
-        
 end
