efa1e08ad46d0ed9cd54ade88e85be2b0452012c
suren <suren@goyaka.com>
2012-05-02 11:50:20 +0530

    Adding Log4r to the project
    OMG, Log4j is awesome :P

---
+++ config/initializers/logger.rb
@@ -0,0 +1,17 @@
+require 'log4r'
+include Log4r
+
+module Log
+  LOGGER = Log4r::Logger.new 'logger'
+  LOGGER.outputters = Log4r::FileOutputter.new 'debugger', :filename => File.join(Rails.root, "log", "#{Rails.env}-debug.log")
+  
+  def Log.method_missing(method, *args, &block)
+    LOGGER.send method, args[0]
+    return
+  end
+  
+  def Log.level(level)
+    LOGGER.level = level
+  end
+        
+end
