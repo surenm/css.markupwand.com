461dbb71e8a2f7f736a6ee3a572a03c8fff28e2c
suren <suren@goyaka.com>
2012-05-02 12:59:55 +0530

    Regrouping is explicit

--- app/tasks/analyzer.rb
+++ app/tasks/analyzer.rb
@@ -1,21 +1,19 @@
 require "pp"
 
 class Analyzer  
-  def self.generate_html(dom)
-    html = dom.render_to_html
-    return html
-  end
   
   def self.analyze(psd_json_data)
     psd_layers = JSON.parse psd_json_data, :symbolize_names => true
     
+    Log.info "Beginning analyzing..."
     dom = PhotoshopItem::Dom.create_dom_from_psd psd_layers
-        
-    html = self.generate_html dom
+    dom.regroup!
+    
+    Log.info "Generating HTML..."
+    html = dom.render_to_html
     html_fptr = File.new '/tmp/result.html', 'w+'
     html_fptr.write html
     html_fptr.close
-    
     return
   end
 end
