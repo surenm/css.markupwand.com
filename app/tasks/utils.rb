faf3f257c7d12a540b18054b4f1f28e65627c008
Alagu <allagappan@gmail.com>
2012-05-05 00:40:38 -0700

    Pull the html, get the css. Stitch together. HTMl ready!

--- app/tasks/utils.rb
+++ app/tasks/utils.rb
@@ -7,7 +7,25 @@ class Utils
     Log.info "Beginning to process #{file_name}..."
     fptr = File.new file_name
     json_data = fptr.read
-    Analyzer.analyze json_data
-    Log.info "Successfully completed processing #{file_name}."
+    html = Analyzer.analyze json_data
+    
+    better_file_name = file_name.split('/').last.gsub('.psd','').gsub('.json','').underscore.gsub(' ','_')
+    folder_path      = Rails.root.join("generated", better_file_name)
+    css_path         = folder_path.join("assets","css")
+    css_file         = css_path.join "style.css"
+    css_data         = PhotoshopItem::StylesHash.generate_css_file
+    File.open(css_file, 'w') {|f| f.write(css_data) }
+    
+    Log.info "Creating css_path #{folder_path}"
+    FileUtils.mkdir_p css_path
+    
+    html_file_name = folder_path.join 'index.html'
+    html_fptr      = File.new html_file_name, 'w+'
+    html_fptr.write html
+    html_fptr.close
+    
+    Log.info "Successfully completed processing #{better_file_name}."
+    
+    return
   end
 end
