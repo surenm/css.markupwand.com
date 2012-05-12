b9fe66eded7124c04490baabae3e1c6746f792fb
suren <suren@goyaka.com>
2012-05-13 04:21:36 +0530

    Colored logs FTW

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -4,7 +4,16 @@ include Log4r
 module Log
   LOGGER = Log4r::Logger.new 'logger'
   LOGGER.outputters = Log4r::FileOutputter.new 'debugger', :filename => File.join(Rails.root, "log", "#{Rails.env}-debug.log")  
-  LOGGER.add Log4r::Outputter.stdout
+
+  LOGGER.add ColorOutputter.new 'color', {:colors => 
+    { 
+      :debug  => :light_blue, 
+      :info   => :green, 
+      :warn   => :yellow, 
+      :error  => :red, 
+      :fatal  => {:color => :red, :background => :white} 
+    } 
+  }
   
   def Log.method_missing(method, *args, &block)
     LOGGER.send method, args[0]
