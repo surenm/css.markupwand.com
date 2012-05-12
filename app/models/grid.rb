325fb0325950510aa5d9d107f408d6d90a749b35
suren <suren@goyaka.com>
2012-05-12 20:33:58 +0530

    Remove css class

--- app/models/grid.rb
+++ app/models/grid.rb
@@ -228,8 +228,7 @@ class Grid
       css.update layer.get_css({}, @is_root)
     end
     
-    css_class = PhotoshopItem::StylesHash.add_and_get_class Converter::to_style_string css    
-    Log.warn css
+    css_class = PhotoshopItem::StylesHash.add_and_get_class Converter::to_style_string css
 
     # Is this required for grids?
     inner_html = args.fetch :inner_html, ''
