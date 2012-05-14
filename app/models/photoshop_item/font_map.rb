2b34c24e9295e8703568cb26d94e89988e360d7d
Alagu <allagappan@gmail.com>
2012-05-14 09:17:42 -0700

    Font map

---
+++ app/models/photoshop_item/font_map.rb
@@ -0,0 +1,117 @@
+class PhotoshopItem::FontMap
+  
+  
+  FONT_MAP = {
+    'Helvetica World' => 'Helvetica'
+  }
+  
+  # Base fonts for browsers
+  # http://en.wikipedia.org/wiki/Core_fonts_for_the_Web
+  DEFAULT_FONTS = ['Helvetica', 'Lucida Sans', 'Tahoma', 'Andale Mono',
+     'Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', 'Georgia',
+     'Impact', 'Times New Roman', 'Trebuchet MS', 'Verdana', 'Webdings']
+  
+  def self.find_web_fonts(layers)
+    fonts_list = []
+    layers.each do |layer_id, layer|
+       fonts_list.push PhotoshopItem::FontMap.get_font_name(layer)
+    end
+    
+    fonts_list.uniq!
+    fonts_list.compact!
+    fonts_list = fonts_list - DEFAULT_FONTS
+    
+    fonts_list
+    
+    typekit_fonts = PhotoshopItem::FontMap.find_in_typekit(fonts_list)
+    google_fonts  =  PhotoshopItem::FontMap.find_in_google_webfonts(fonts_list)
+    
+    webfonts = {}
+    fonts_list.each do |font|
+      pp typekit_fonts[font]
+      pp google_fonts[font]
+      webfonts[font] = typekit_fonts[font] + google_fonts[font]
+    end
+    
+    pp webfonts
+    
+  end
+  
+  def self.find_in_typekit(fonts_list)
+    typekit_folder = Rails.root.join('db','json','typekit_fonts')
+    files = Dir.new(typekit_folder).entries
+    files.slice! 0, 2 # Remove '.' and '..'
+    font_matches = {}
+    
+    # Create empty hash
+    fonts_list.each { |font| font_matches[font] = [] }
+    
+    
+    files.each do |file|
+      file_path = typekit_folder.join(file)
+      font_data = JSON.parse(File.open(file_path).read)
+      library_type = font_data['library']['name']
+      
+      fonts_list.each do |font_name|
+        matches = font_data['library']['families'].find_all do |typekit_font|
+          typekit_font['name'] =~ /#{font_name}/i
+        end 
+        
+        matches.each_with_index do |item,index|
+          matches[index]['library_type'] = library_type
+          matches[index]['source'] = 'typekit'
+        end
+        
+        font_matches[font_name] += matches
+      end
+    end
+    puts "2"
+    
+    font_matches
+  end
+  
+  def self.find_in_google_webfonts(fonts_list)
+    google_folder =  Rails.root.join('db','json','google_webfonts')
+    files = Dir.new(google_folder).entries
+    files.slice! 0, 2 # Remove '.' and '..'
+    
+    font_matches = {}
+    # Create empty hash
+    fonts_list.each { |font| font_matches[font] = [] }
+    
+    files.each do |file|
+      file_path = google_folder.join(file)
+      font_data = JSON.parse(File.open(file_path).read)
+      
+      fonts_list.each do |font_name|
+        matches = font_data['items'].find_all do |google_font|
+          google_font['family'] =~ /#{font_name}/i
+        end
+        
+        matches.each_with_index do |item,index|
+          matches[index]['name'] = matches[index]['name']
+          matches[index]['source'] = 'google'
+        end
+        
+        font_matches[font_name] += matches
+      end
+    end
+    puts "3"
+    
+    font_matches
+    
+  end
+  
+  def self.get_font_name(layer)
+    if layer[:layerKind] == PhotoshopItem::Layer::LAYER_TEXT
+      text_style = layer[:textKey][:value][:textStyleRange][:value].first
+      font_info  = text_style[:value][:textStyle][:value]
+      font_name  = font_info[:fontName][:value]
+      if FONT_MAP.has_key? font_name
+        FONT_MAP[font_name]
+      else
+        font_name
+      end
+    end
+  end
+end
