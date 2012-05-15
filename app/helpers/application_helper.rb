3c3fba683618e117e27af1f4e54b8b4362f80b88
Alagu <allagappan@gmail.com>
2012-05-14 17:37:54 -0700

    Get json from a specific url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
+require 'net/http'
+require 'json'
+
 module ApplicationHelper
+  def ApplicationHelper::get_json(url)
+    url = URI.parse(url)
+    req = Net::HTTP::Get.new(url.path)
+    res = Net::HTTP.start(url.host, url.port) do |http|
+      http.request(req)
+    end
+    
+    JSON.parse res.body
+  end
 end
