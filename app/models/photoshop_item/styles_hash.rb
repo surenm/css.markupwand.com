a70c2c5a22f77f13d638c91d3347680d212d113a
Alagu <allagappan@gmail.com>
2012-05-14 17:47:27 -0700

    Change styles hash from static to instance

--- app/models/photoshop_item/styles_hash.rb
+++ app/models/photoshop_item/styles_hash.rb
@@ -1,21 +1,14 @@
 class PhotoshopItem::StylesHash
-  def initialize
-    @styles = Hash.new
+  
+  attr_accessor :styles, :index
+  
+  def initialize(font_map)
+    @css_classes = Hash.new
     @index  = 0
   end
   
-  def styles
-    @styles
-  end
-  
-  def self.debug
+  def debug
     ENV.has_key? 'DEBUG' and ENV['DEBUG'].to_i > 0
-  end
-  
-  @@instance = PhotoshopItem::StylesHash.new
-  
-  def self.instance
-    return @@instance
   end
   
   def add_and_get_class(css)
@@ -23,42 +16,32 @@ class PhotoshopItem::StylesHash
       return nil
     end
     
-    if @styles.has_key? css
-      return @styles[css]
+    if @css_classes.has_key? css
+      return @css_classes[css]
     else
       @index = @index + 1
       class_name = 'class' + (@index).to_s
-      @styles[css] = class_name
+      @css_classes[css] = class_name
       return class_name
     end
   end
   
-  def self.get_styles_hash
-    PhotoshopItem::StylesHash.instance.styles
-  end
-  
-  def self.add_and_get_class(string)
-    PhotoshopItem::StylesHash.instance.add_and_get_class(string)
-  end
-  
-  def self.write_css_file(folder_path)
+  def write_css_file(folder_path)
     css_path = folder_path.join("assets", "css")
     FileUtils.mkdir_p css_path
     
     css_file = css_path.join "style.css"
-    css_data = PhotoshopItem::StylesHash.generate_css_file
-    
     Log.info "Writing css file #{css_file}"
     
-    File.open(css_file, 'w') {|f| f.write(css_data) }
+    File.open(css_file, 'w') {|f| f.write(generate_css_data) }
   end
   
-  def self.generate_css_file
-    classes  = PhotoshopItem::StylesHash.get_styles_hash
+  def generate_css_data
+   
     css_data = ''
     
     # Debugging div positioning
-    if self.debug
+    if debug
       css_data += <<DIV_BLOCK
   div {
     border: 1px Gray dotted;
@@ -66,7 +49,7 @@ class PhotoshopItem::StylesHash
 DIV_BLOCK
     end
       
-    classes.each do |style, class_name|
+    @css_classes.each do |style, class_name|
       style_formatted = style.gsub(";",";\n")
       class_block = <<CLASS_BLOCK
 .#{class_name} {
