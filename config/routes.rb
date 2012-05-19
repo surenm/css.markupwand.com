36c6b1c5509299711510e711b54bbc790901e685
suren <suren@goyaka.com>
2012-05-20 04:42:08 +0530

    Organize routes.rb a bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 TransformersWeb::Application.routes.draw do
   resources :design_files
-  match "/designs/*path" => "design_files#serve"
-  match 'next_unprocessed' => "design_files#next_unprocessed"
-  match '/upload' => "design_files#create"
-  match 'edit' => 'main#edit'
+  match '/designs/*path'      => "design_files#serve"
+  match 'next_unprocessed'    => "design_files#next_unprocessed"
+  match '/upload'             => "design_files#create"
   
+  # Annotator interface
+  match 'edit'                => 'main#edit'
+  
+  # Proxy method to view generated files
   match 'generated/*uri.*ext' => 'main#generated'
   
+  # Main page redirects to index
   root :to => 'main#index'
 end
