deab0955f283c34a32be1b02b38cc24a6129f191
Rajagopal Natarajan <raj@goyaka.com>
2012-05-26 16:50:33 -0700

    Config changes to make the lib files autoloaded

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module TransformersWeb
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += Dir["#{config.root}/app/tasks/**/"]
+    config.autoload_paths += Dir["#{config.root}/app/tasks/**/", "#{config.root}/lib/**/"]
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
