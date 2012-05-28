53a9f669f2ccff0fbccb3086cf2c2941525e1cb8
suren <suren@goyaka.com>
2012-05-28 19:07:48 +0530

    AssetSyncing in application.rb itself

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,28 @@ module TransformersWeb
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
+    
+    AssetSync.configure do |config|
+      config.fog_provider = 'AWS'
+      config.fog_directory = ENV['FOG_DIRECTORY']
+      config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
+      config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+
+      # Don't delete files from the store
+      config.existing_remote_files = "keep"
+
+      # Increase upload performance by configuring your region
+      # config.fog_region = 'eu-west-1'
+      #
+      # Automatically replace files with their equivalent gzip compressed version
+      config.gzip_compression = true
+
+      # Use the Rails generated 'manifest.yml' file to produce the list of files to 
+      # upload instead of searching the assets directory.
+      config.manifest = true
+
+      # Fail silently.  Useful for environments such as Heroku
+      # config.fail_silently = true
+    end
   end
 end
