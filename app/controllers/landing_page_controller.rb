3fe5185d285d0848a6f3f19ab5f387a631ae166f
Rajagopal Natarajan <raj@goyaka.com>
2012-05-28 00:16:49 -0700

    Validation, db update and foo

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -4,5 +4,12 @@ class LandingPageController < ApplicationController
   
   def getinvite
     @email = params[:email]
+    users = User.where(:email => @email)
+    if(users.size==0)
+      name = @email
+      name.slice!(/\@.*/)
+      user = User.create!({:email=>@email, :name=>name})
+      user.save
+    end
   end
 end
