5eb0b79e552ffcbdbc9b62dde5ec53ec77a3a2e9
suren <suren@goyaka.com>
2012-05-28 18:09:22 +0530

    Fixing asset syncing

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,4 @@
 CarrierWave.configure do |config|
-    config.grid_fs_connection = Mongoid.database
-    config.storage = :grid_fs
+  config.grid_fs_connection = Mongoid.database
+  config.storage = :grid_fs
 end
