24beae675c923d9fae0ceedb40c85ce880de3a86
suren <suren@goyaka.com>
2012-06-03 20:26:06 +0530

    Store can write to local or S3

--- lib/store.rb
+++ lib/store.rb
@@ -9,7 +9,7 @@ module Store
     "#{Store::BUCKET_ROOT}_#{Rails.env}"
   end
   
-  def Store::get_s3_bucket
+  def Store::get_S3_store
     bucket_name = Store::get_s3_bucket_name
     bucket = Store::S3.buckets[bucket_name]
     if not bucket.exists?
@@ -18,11 +18,37 @@ module Store
     return bucket
   end
   
-  def Store::write(design, file_name, file_contents)
-    key = "#{design.user.email}/#{design.safe_name_prefix}-#{design.id}/#{file_name}"
-    bucket = Store::get_s3_bucket
-    file = bucket.objects[key]
-    file.write file_contents
-    return key
+  def Store::get_local_store
+    root_dir = File.join Rails.root.to_s, 'store'
+    if not Dir.exists? root_dir 
+      Dir.mkdir root_dir
+    end
+    root_dir
+  end
+  
+  def Store::write_to_S3(file_key, file_contents)
+    s3_bucket = Store::get_S3_store
+    fptr = s3_bucket.objects[file_key]
+    fptr.write file_contents
+  end
+  
+  def Store::write_to_local(file_key, file_contents)
+    local_store = Store::get_local_store
+    file_path = File.join local_store, file_key
+    file_dir = File.dirname file_path
+    if not Dir.exists? file_dir
+      FileUtils.mkdir_p file_dir
+    end
+    fptr = File.open file_path, 'wb'
+    fptr.write file_contents
+    fptr.close
+  end
+  
+  def Store::write(file_key, file_contents)
+    if Rails.env == "production" or ENV['UPLOAD_TO_AWS'] == true
+      Store::write_to_S3 file_key, file_contents
+    else 
+      Store::write_to_local file_key, file_contents
+    end
   end
 end
