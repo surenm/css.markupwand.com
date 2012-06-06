6c7f4f28e00e6cb056f4dfd871468af240db7343
suren <suren@goyaka.com>
2012-06-06 15:21:38 +0530

    Enable mock mode for FOG

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,10 @@
+Fog.mock!
+connection = Fog::Storage.new(:provider => 'AWS', 
+:aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+:aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
+)
+connection.directories.create(:key => 'store_development')
+
 CarrierWave.configure do |config|
   
   config.fog_credentials = {
