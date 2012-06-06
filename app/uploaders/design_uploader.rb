364b48fc49d41f36968b0da8f2db799ae155d779
suren <suren@goyaka.com>
2012-06-06 18:30:30 +0530

    DesignUploader is a bloody two face now.

--- app/uploaders/design_uploader.rb
+++ app/uploaders/design_uploader.rb
@@ -1,8 +1,17 @@
-class DesignUploader < CarrierWave::Uploader::Base
-  storage :fog
-  
-  def store_dir
-    "uploads/#{model.id.to_s}"
+if Constants::store_remote?
+  class DesignUploader < CarrierWave::Uploader::Base
+    include CarrierWaveDirect::Uploader
+    
+    include ActiveModel::Conversion
+    extend ActiveModel::Naming
   end
-  
+else
+  class DesignUploader < CarrierWave::Uploader::Base
+    storage :file
+    
+    def store_dir
+      "uploads/#{model.id.to_s}"
+    end
+  end
 end
+
