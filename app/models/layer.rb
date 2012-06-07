d6971123e6a7adc50c7e0096ab5c93e617b70d6a
Alagu <allagappan@gmail.com>
2012-06-06 19:24:56 -0700

    bug - added a comma in the last.

--- app/models/layer.rb
+++ app/models/layer.rb
@@ -27,8 +27,8 @@ class Layer
 
   # Do not store layer_object, but have in memory
   
-  attr_accessor :layer_object, :bounds, :intersect_count, :overlays, 
-  
+  attr_accessor :layer_object, :bounds, :intersect_count, :overlays
+
   def self.create_from_raw_data(layer_json)
     layer = Layer.new
     layer.set layer_json
