cd848ee0ac8e83750721427f2a57cf6531dc06c2
suren <suren@goyaka.com>
2012-06-13 00:52:13 +0530

    Generate markup from processed files

--- app/controllers/markup_generator_job.rb
+++ app/controllers/markup_generator_job.rb
@@ -3,6 +3,22 @@ class MarkupGeneratorJob
   
   def self.perform(design_id)
     design = Design.find design_id
-    Log.info design
+    design.set_status Design::STATUS_GENERATING
+    
+    Store::fetch_from_store design.store_key_prefix
+
+    design_processed_directory = Rails.root.join 'tmp', 'store', design.store_key_prefix, 'processed'
+    Log.info design_processed_directory
+    
+    Dir["#{design_processed_directory}/*.psd.json"].each do |processed_file|
+      Log.info "Found processed file - #{processed_file}"
+      design.processed_file_path = processed_file
+      design.save!
+      break
+    end
+    
+    design.parse
+    design.generate_markup
+    design.setstatus Design::STATUS_COMPLETED
   end
 end
