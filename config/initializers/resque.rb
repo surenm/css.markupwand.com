1871d7e1d8dbf2ae40fdc5fe99d160bad890b54d
suren <suren@goyaka.com>
2012-06-12 23:25:54 +0530

    PODA MOKKA NAAYE. WORK PANNI THOLAYEN

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,8 +1,6 @@
 require 'resque/server'
 
-config_file   = Rails.root.join "config", "resque.yml"
-resque_config = YAML.load_file(config_file)[Rails.env]
+ENV["REDISTOGO_URL"] ||= "redis://localhost:6379"
 
-uri = URI.parse(resque_config["redis_uri"])
-Log.info uri
-Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+uri = URI.parse(ENV["REDISTOGO_URL"])
+Resque.redis = Redis.new :host => uri.host, :port => uri.port, :password => uri.password
