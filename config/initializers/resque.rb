c72f896956b4553ce21bd8787fe2b0bae492741f
suren <suren@goyaka.com>
2012-06-12 22:54:22 +0530

    Fix red is to load properly for heroku

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,4 +3,6 @@ require 'resque/server'
 config_file   = Rails.root.join "config", "resque.yml"
 resque_config = YAML.load_file(config_file)[Rails.env]
 
-Resque.redis  = resque_config['redis_uri']
+uri = URI.parse(resque_config["redis_uri"])
+Log.info uri
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
