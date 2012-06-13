1f8963483fe9db5c1f0885fd11cb39f65fc7c488
Rajagopal Natarajan <raj@goyaka.com>
2012-06-13 11:58:22 -0700

    A couple of nil error fixes done yesterday

---
+++ lib/overrides/array.rb
@@ -0,0 +1,18 @@
+class Array
+  def extract_value(*keys)
+    return nil if keys.nil? or keys.empty?
+    #FIXME: Need to add checks to validate the keys further
+
+    keys.each_with_index do |key, i|
+      if self.nil?
+        return nil
+      end
+      if self[key].class != Array and self[key].class != Hash or i == keys.size-1
+        return self[key]
+      else
+        remaining_keys = keys.slice(i+1, keys.size-i-1)
+        return self[key].extract_value(*remaining_keys)
+      end
+    end
+  end
+end
