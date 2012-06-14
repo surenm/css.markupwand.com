c85e76d7168ec121da1dd180c60384e84955a292
suren <suren@goyaka.com>
2012-06-14 12:19:21 +0530

    Just fetch processed folder to process

--- app/tasks/markup_generator_job.rb
+++ app/tasks/markup_generator_job.rb
@@ -5,12 +5,9 @@ class MarkupGeneratorJob
     design = Design.find design_id
     design.set_status Design::STATUS_GENERATING
     
-    Store::fetch_from_store design.store_key_prefix
-
     Store::fetch_from_store design.store_processed_key
 
     design_processed_directory = Rails.root.join 'tmp', 'store', design.store_processed_key
-    Log.info design_processed_directory
     
     Dir["#{design_processed_directory}/*.psd.json"].each do |processed_file|
       Log.info "Found processed file - #{processed_file}"
