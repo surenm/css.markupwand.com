6293ad79eb665151fb22a97c12cd1ab90d083b3e
suren <suren@goyaka.com>
2012-06-15 12:02:16 +0530

    What if the tag was not passed by grid?

--- app/models/layer.rb
+++ app/models/layer.rb
@@ -250,8 +250,7 @@ class Layer
     css       = args.fetch :css, {}
     css_class = class_name css, is_leaf, @is_root, grid
     
-    tag = tag_name(is_leaf)
-    tag = args.fetch :tag
+    tag = args.fetch :tag, self.tag_name(is_leaf)
 
     inner_html = args.fetch :inner_html, ''
     if inner_html.empty? and self.kind == LAYER_TEXT
