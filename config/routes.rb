02b1375c0795a2e95c7b11b7d6ca881bc5de3051
Alagu <allagappan@gmail.com>
2012-06-17 18:43:51 -0700

    Both routes for prod and dev

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,14 @@ TransformersWeb::Application.routes.draw do
   match 'design/new'       => 'design#new'
   match 'design/uploaded'  => 'design#uploaded', :as => :uploaded_callback
 
+  if Rails.env.production?
+    match 'design/new'       => 'design#new'
+    match 'design/uploaded'  => 'design#uploaded', :as => :uploaded_callback
+  else
+    match 'design/new'       => 'design#local_new'
+    match 'design/uploaded'  => 'design#local_uploaded', :as => :uploaded_callback
+  end
+  
   # photoshop machines will ping back on this url
   match 'design/processed' => 'design#processed', :as => :processed_callback
 
