cc0cdb6f602c56e530b9c2013e9c1bc0cbcf96eb
suren <suren@goyaka.com>
2012-06-19 21:40:59 +0530

    No more force stuff
    and mail only in production

--- app/tasks/markup_generator_job.rb
+++ app/tasks/markup_generator_job.rb
@@ -11,14 +11,16 @@ class MarkupGeneratorJob
     Store::fetch_from_store design.store_processed_key
 
     # Generate markup once in publishable mode once
-    design.generate_markup :enable_data_attributes => false, :force => true
+    #design.generate_markup :enable_data_attributes => false
     
     # Generate markup once in editable mode once
-    design.generate_markup :enable_data_attributes => true, :force => true
+    design.generate_markup :enable_data_attributes => true
 
     # mark editing complete
     design.set_status Design::STATUS_COMPLETED
 
-    ApplicationHelper.post_simple_message("#{design.user.name} <#{design.user.email}>", "#{design.name} generated", "Click -> http://www.markupwand.com/design/#{design.safe_name}/edit")
+    if Rails.env.production?
+      ApplicationHelper.post_simple_message("#{design.user.name} <#{design.user.email}>", "#{design.name} generated", "Click -> http://www.markupwand.com/design/#{design.safe_name}/edit")
+    end
   end
 end
