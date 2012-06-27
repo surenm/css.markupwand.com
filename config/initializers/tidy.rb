3c435a4d54dd10d24efadde3415bb22bdc7e4e49
suren <suren@goyaka.com>
2012-06-27 16:53:59 +0530

    Tidy made available in staging as well

--- config/initializers/tidy.rb
+++ config/initializers/tidy.rb
@@ -1,5 +1,5 @@
 require 'tidy_ffi'
-if Rails.env.production?
+if Rails.env.production? or Rails.env.staging?
   TidyFFI.library_path = Rails.root.join("lib", "native", "libtidy.so").to_s
   require 'tidy_ffi/interface'
   require 'tidy_ffi/lib_tidy'
