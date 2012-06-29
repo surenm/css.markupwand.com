10511f4a96f1f891dcf477ba43d3d4cdb8615be8
suren <suren@goyaka.com>
2012-06-29 16:30:56 +0530

    Unknown RubyProf module in production

--- config/initializers/04_profiler.rb
+++ config/initializers/04_profiler.rb
@@ -9,18 +9,22 @@ module Profiler
   end
   
   def Profiler::start
-    if Profiler::profiling_enabled? and not RubyProf.running?
-      RubyProf.start
+    if Profiler::profiling_enabled? # should not even get inside if profiler is disabled
+      if not RubyProf.running?
+        RubyProf.start
+      end
     end
   end
   
   def Profiler::stop
-    if RubyProf.running?
-      result       = RubyProf.stop
-      printer      = RubyProf::GraphHtmlPrinter.new result
-      profile_file = File.new Rails.root.join("tmp", "profile-#{Time.now}.html"), 'w+'
-      printer.print profile_file, :min_percent => 10
-      profile_file.close
+    if Profiler::profiling_enabled?
+      if RubyProf.running?
+        result       = RubyProf.stop
+        printer      = RubyProf::GraphHtmlPrinter.new result
+        profile_file = File.new Rails.root.join("tmp", "profile-#{Time.now}.html"), 'w+'
+        printer.print profile_file, :min_percent => 10
+        profile_file.close
+      end
     end
   end
 end
