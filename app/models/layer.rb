1c6dcd8069d234ac39844fa4c9aef57d193b02a2
Rajagopal Natarajan <raj@goyaka.com>
2012-06-30 15:36:48 -0700

    Cropping image to container dimensions -  complete

--- app/models/layer.rb
+++ app/models/layer.rb
@@ -1,4 +1,4 @@
-require'RMagick'
+require 'RMagick'
 include Magick
 
 class Layer
@@ -7,29 +7,29 @@ class Layer
   include Mongoid::Timestamps::Updated
   include ActionView::Helpers::TagHelper
 
-  LAYER_TEXT          = "LayerKind.TEXT"
-  LAYER_SMARTOBJECT   = "LayerKind.SMARTOBJECT"
-  LAYER_SOLIDFILL     = "LayerKind.SOLIDFILL"
-  LAYER_NORMAL        = "LayerKind.NORMAL"
+  LAYER_TEXT = "LayerKind.TEXT"
+  LAYER_SMARTOBJECT = "LayerKind.SMARTOBJECT"
+  LAYER_SOLIDFILL = "LayerKind.SOLIDFILL"
+  LAYER_NORMAL = "LayerKind.NORMAL"
   LAYER_HUESATURATION = "LayerKind.HUESATURATION"
-  
+
   BOUND_MODES = {
-    :NORMAL_BOUNDS => :bounds,
-    :SMART_BOUNDS => :smart_bounds,
-    :EDGE_BOUNDS => :edge_detected_bounds, 
-    :SNAPPED_BOUNDS => :snapped_bounds
-    }
+      :NORMAL_BOUNDS => :bounds,
+      :SMART_BOUNDS => :smart_bounds,
+      :EDGE_BOUNDS => :edge_detected_bounds,
+      :SNAPPED_BOUNDS => :snapped_bounds
+  }
 
   has_and_belongs_to_many :grids, :class_name => 'Grid'
   belongs_to :design
 
-  field :uid, :type  => String
+  field :uid, :type => String
   field :name, :type => String
   field :kind, :type => String
-  field :raw, :type  => String
+  field :raw, :type => String
   field :layer_type, :type => String, :default => nil
   field :is_overlay, :type => Boolean
-  
+
   field :override_tag, :type => String, :default => nil
   field :layer_bounds, :type => String, :default => nil
 
@@ -38,7 +38,7 @@ class Layer
   field :initial_layer_bounds, :type => String, :default => nil
 
   # TOD: Do not store layer_object, but have in memory
-  
+
   attr_accessor :layer_object, :intersect_count, :overlays
 
   def self.create_from_raw_data(layer_json, design_id)
@@ -50,24 +50,24 @@ class Layer
     layer.set layer_json
     return layer
   end
-  
-  
+
+
   def set(layer)
-    self.name       = layer[:name][:value]
-    self.kind       = layer[:layerKind]
+    self.name = layer[:name][:value]
+    self.kind = layer[:layerKind]
     self.layer_type = layer[:layerType]
-    self.uid        = layer[:layerID][:value]
+    self.uid = layer[:layerID][:value]
 
     if self.layer_json[BOUND_MODES[:SMART_BOUNDS]] and not self.layer_json[BOUND_MODES[:SMART_BOUNDS]].empty?
       self.set_bounds_mode(:SMART_BOUNDS)
     end
-    
+
     bounds_key = self.bounds_key
-    value  = self.layer_json[bounds_key][:value]
-    top    = value[:top][:value]
+    value = self.layer_json[bounds_key][:value]
+    top = value[:top][:value]
     bottom = value[:bottom][:value]
-    left   = value[:left][:value]
-    right  = value[:right][:value]
+    left = value[:left][:value]
+    right = value[:right][:value]
 
     if self.initial_bounds.nil?
       initial_bounds = BoundingBox.new(top, left, bottom, right)
@@ -75,25 +75,25 @@ class Layer
     end
 
     design_bounds = BoundingBox.new 0, 0, self.design.height, self.design.width
-    layer_bounds  = BoundingBox.new(top, left, bottom, right).inner_crop(design_bounds)
-    
+    layer_bounds = BoundingBox.new(top, left, bottom, right).inner_crop(design_bounds)
+
     self.layer_bounds = BoundingBox.pickle layer_bounds
     self.save!
   end
-  
+
   # TODO Change object property and initialize when we are making properties inside.
   def zindex
     layer_json.extract_value(:itemIndex, :value)
   end
-  
+
   def attribute_data
     {
-      :uid        => self.uid,
-      :name       => self.name,
-      :kind       => self.kind,
-      :layer_type => self.layer_type,
-      :label      => self.name[0..9],
-      :tag        => self.tag_name
+        :uid => self.uid,
+        :name => self.name,
+        :kind => self.kind,
+        :layer_type => self.layer_type,
+        :label => self.name[0..9],
+        :tag => self.tag_name
     }
   end
 
@@ -118,17 +118,17 @@ class Layer
   end
 
   def has_newline?
-    if self.kind == Layer::LAYER_TEXT and 
-      layer_json.has_key? :textKey and
-      layer_json.extract_value(:textKey, :value).has_key? :textKey
-      
+    if self.kind == Layer::LAYER_TEXT and
+        layer_json.has_key? :textKey and
+        layer_json.extract_value(:textKey, :value).has_key? :textKey
+
       string_data = layer_json[:textKey][:value][:textKey][:value]
       (string_data =~ /\r/) or (string_data =~ /\n/)
-    else      
+    else
       false
     end
   end
-  
+
   def set_bounds_mode(bound_mode)
     unless BOUND_MODES.include? bound_mode
       raise "Unknown bound mode #{bound_mode}"
@@ -148,15 +148,15 @@ class Layer
       design = self.design
       ensure_processed_folder_exists!
 
-      fptr     = File.read design.processed_file_path
+      fptr = File.read design.processed_file_path
       psd_data = JSON.parse fptr, :symbolize_names => true, :max_nesting => false
-      
+
       @layer_object = psd_data[:art_layers].fetch :"#{self.uid}"
     end
-    
+
     @layer_object
   end
-  
+
   def bounds_key
     key = BOUND_MODES[@bound_mode]
     key = BOUND_MODES[:NORMAL_BOUNDS] if key.nil?
@@ -198,7 +198,7 @@ class Layer
   def intersect?(other)
     return self.bounds.intersect? other.bounds
   end
-  
+
   def intersect_area(other)
     return self.bounds.intersect_area other.bounds
   end
@@ -206,7 +206,7 @@ class Layer
   def renderable_image?
     !self.layer_json.nil? and self.layer_json.has_key? :renderImage and self.layer_json[:renderImage]
   end
-  
+
   def unmaskable_layer?
     self.kind == Layer::LAYER_HUESATURATION
   end
@@ -216,34 +216,36 @@ class Layer
   end
 
   def tag_name(is_leaf = false)
+    chosen_tag = ""
     if not self.override_tag.nil?
       self.override_tag
     elsif self.kind == LAYER_SMARTOBJECT
       if is_leaf
-        :img
+        chosen_tag = :img
       else
-        :div
+        chosen_tag = :div
       end
     elsif self.kind == LAYER_NORMAL
       if self.renderable_image? and is_leaf
-        :img
+        chosen_tag = :img
       else
-        :div
+        chosen_tag = :div
       end
-    elsif self.kind  == LAYER_TEXT or self.kind == LAYER_SOLIDFILL
-      :div
+    elsif self.kind == LAYER_TEXT or self.kind == LAYER_SOLIDFILL
+      chosen_tag = :div
     else
       Log.info "New layer found #{self.kind} for layer #{self.name}"
-      :div
+      chosen_tag = :div
     end
+    chosen_tag
   end
 
   def get_css(css = {}, is_leaf, grid)
+    crop_objects_for_cropped_bounds
     if self.kind == LAYER_TEXT
       css.update CssParser::parse_text self
     elsif not is_leaf and (self.kind == LAYER_SMARTOBJECT or renderable_image?)
       #TODO Replace into a css parser function
-      crop_image image_path
       css[:background] = "url('../../#{image_path}') no-repeat"
       css[:'background-size'] = "contain"
       if grid
@@ -253,7 +255,7 @@ class Layer
     elsif self.kind == LAYER_SOLIDFILL
       css.update CssParser::parse_shape self, grid
     end
-    
+
     css
   end
 
@@ -266,16 +268,16 @@ class Layer
     font_name = nil
 
     if layer_json[:layerKind] == Layer::LAYER_TEXT
-      font_name  = layer_json.extract_value(:textKey, :value, :textStyleRange, :value)[0].extract_value(:value, :textStyle, :value, :fontName, :value)
-    end    
-    
+      font_name = layer_json.extract_value(:textKey, :value, :textStyleRange, :value)[0].extract_value(:value, :textStyle, :value, :fontName, :value)
+    end
+
     font_name
   end
-  
+
   def get_font_name(position)
     raw_font_name = self.get_raw_font_name
     return nil if raw_font_name.nil?
-    
+
     design = self.grids.first.design
     font_map = design.font_map
     if font_map.has_key? raw_font_name
@@ -283,17 +285,17 @@ class Layer
     else
       font_name = raw_font_name
     end
-    
+
     font_name
   end
-  
+
   # A Layer whose bounds are zero
   def empty?
     is_empty = false
     if self.bounds
-     is_empty =  ((self.bounds.top + self.bounds.left + self.bounds.bottom + self.bounds.right) == 0)
+      is_empty = ((self.bounds.top + self.bounds.left + self.bounds.bottom + self.bounds.right) == 0)
     end
-    
+
     is_empty
   end
 
@@ -324,13 +326,13 @@ class Layer
         chunks.each_with_index do |chunk, index|
           next if chunk.length == 0
           attributes = {:class => text_chunk_class(index)}
-          multifont_text +=  content_tag :span, chunk, attributes
+          multifont_text += content_tag :span, chunk, attributes
         end
 
         multifont_text
 
       else
-        original_text        
+        original_text
       end
     else
       ''
@@ -359,32 +361,40 @@ class Layer
     image_height = current_image.rows
 
     Log.info "Checking if the image in disk is bigger than the desired size"
-    Log.info "Image size #{image_width} #{image_height}"
 
-    if image_width > self.bounds.width and image_height > self.bounds.height
+    if image_width >= self.bounds.width and image_height >= self.bounds.height
+      Log.info "Yes it is! Cropping..."
+
+      top_offset = (self.bounds.top - self.initial_bounds.top).abs
+      left_offset = (self.bounds.left - self.initial_bounds.left).abs
+
+      Log.info "Cropping #{current_image_path} at offsets - top: #{top_offset} left: #{left_offset}"
+      current_image.crop!(left_offset, top_offset, self.bounds.width, self.bounds.height)
+      current_image.write(current_image_path)
+
+      Log.info "Saving to the store"
+      Store::save_to_store current_image_path, File.join(design.store_processed_key, image_name)
+    elsif  image_width < self.bounds.width and image_height < self.bounds.height
+      Log.error "Looks like the image has been cropped to a smaller size than desired."
+      return
+    else
       Log.info "Looks like someone beat me to cropping it. Not cropping again."
       return
     end
+  end
 
-    Log.info "Yes it is! Cropping..."
-
-    top_offset = (self.bounds.top - self.initial_bounds.top).abs
-    left_offset = (self.bounds.left - self.initial_bounds.left).abs
-
-    Log.info "Cropping #{current_image_path}"
-    current_image.crop!(left_offset, top_offset, self.bounds.width, self.bounds.height)
-    current_image.write(current_image_path)
-
-    Log.info "Saving to the store"
-    Store::save_to_store current_image_path, File.join(design.store_processed_key, image_name)
-
+  def crop_objects_for_cropped_bounds
+    if self.renderable_image?
+      crop_image image_path
+    end
   end
 
   def to_html(args = {}, is_leaf, grid)
+
     Log.info "[HTML] Layer #{self.to_s}"
-    css       = args.fetch :css, {}
+    css = args.fetch :css, {}
     css_class = class_name css, is_leaf, @is_root, grid
-    
+
     generated_tag = tag_name(is_leaf)
     tag = args.fetch :tag, generated_tag
 
@@ -393,18 +403,14 @@ class Layer
       inner_html = text
     end
 
-    attributes         = Hash.new
-    css_class_list     = (args.has_key? :class ) ? [args[:class]] : []
+    attributes = Hash.new
+    css_class_list = (args.has_key? :class) ? [args[:class]] : []
     css_class_list.push css_class
     attributes[:class] = css_class_list.join ' '
 
-    attributes[:"data-grid-id"]  = args.fetch :"data-grid-id", "" 
+    attributes[:"data-grid-id"] = args.fetch :"data-grid-id", ""
     attributes[:"data-layer-id"] = self.id.to_s
     attributes[:"data-layer-name"] = self.name
-
-    if self.renderable_image?
-      crop_image(image_path)
-    end
 
     if tag == :img
       attributes[:src] = image_path
@@ -415,18 +421,18 @@ class Layer
 
     return html
   end
-  
+
   def styleable_layer?
     (self.kind == Layer::LAYER_SOLIDFILL or
-     self.kind == Layer::LAYER_HUESATURATION or
-     self.kind == Layer::LAYER_NORMAL or
-     self.renderable_image?)
+        self.kind == Layer::LAYER_HUESATURATION or
+        self.kind == Layer::LAYER_NORMAL or
+        self.renderable_image?)
   end
 
   def to_s
     "#{self.name} - #{self.bounds} - #{self.kind}"
   end
-  
+
   def inspect
     self.to_s
   end
@@ -434,7 +440,7 @@ class Layer
   def print(indent_level = 0)
     spaces = ""
     prefix = "|--"
-    indent_level.times {|i| spaces+=" "}
+    indent_level.times { |i| spaces+=" " }
     Log.debug "#{spaces}#{prefix} (layer) #{self.name} #{@bounds.to_s}"
   end
 
