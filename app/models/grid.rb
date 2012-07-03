a46893606136eb91a4e5d795915c0305d016f093
suren <suren@goyaka.com>
2012-07-03 15:36:49 +0530

    Remove resolve intersection method

--- app/models/grid.rb
+++ app/models/grid.rb
@@ -431,58 +431,9 @@ class Grid
     [smaller_node, bigger_node]
   end
   
-  def resolve_intersecting_nodes(grid, nodes_in_region)
-    intersecting_node_pairs = Grid.get_intersecting_nodes nodes_in_region
-    Log.info "Intersecting layers found - #{intersecting_node_pairs}"
     
-    if not intersecting_node_pairs.empty?
-      intersecting_nodes = intersecting_node_pairs.flatten!
-    
-      # These are positioned layers
-      # Sort Layers by their layer index.
-      # Keep appending them
-      intersecting_nodes.sort! { |node1, node2|  node2.zindex <=> node1.zindex }
       
-      intersecting_nodes.each { |node| node.overlays = [] }
-
-      # If some layerx is intersecting other layery, then layery needs to be just there,
-      # layerx needs to be relatively positioned.
-      # So, add layerx to layery's list of intersecting nodes.
-      intersecting_nodes.each do |intersector|
-        intersecting_nodes.each do |target|
-          if intersector.intersect? target and
-            intersector[:uid] != target[:uid] and
-            not intersector.overlays.include? target[:uid]
-            intersector.is_overlay = true
-            target.overlays.push intersector[:uid]
-          end
-        end
-      end
       
-      # Once information is set that they are overlaid, remember them.
-      positioned_layers = intersecting_nodes.select { |node| node.is_overlay == true }
-      positioned_layers.sort! { |layer1, layer2| layer2.zindex <=> layer1.zindex }
-      positioned_layers_children = []
-      positioned_layers.each do |layer|
-        positioned_grid = Grid.new :design => self.design
-        nodes_in_grid = BoundingBox.get_nodes_in_region layer.bounds, nodes_in_region, layer.zindex
-        positioned_layers_children = positioned_layers_children | nodes_in_grid
-        nodes_in_region = nodes_in_region - nodes_in_grid
-        positioned_grid.depth = self.depth + 1
-        positioned_grid.set nodes_in_grid, self
-        positioned_grid.is_positioned = true
-        positioned_grid.save!
-        @@grouping_queue.push positioned_grid
-      end
-      
-      self.save!
-      
-      normal_layout_nodes = (nodes_in_region - positioned_layers - positioned_layers_children)
-      
-      return normal_layout_nodes, (positioned_layers | positioned_layers_children)
-    else
-      Log.info "No intersecting node found, and no nodes reduced as well"
-      return nodes_in_region, []
     end
   end
   
