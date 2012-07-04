35c8f1d5dbadf1005516b536852b6cea09d55fd9
Alagu <allagappan@gmail.com>
2012-07-03 20:44:12 -0700

    Fix error, will merge from master later

--- lib/store.rb
+++ lib/store.rb
@@ -212,9 +212,9 @@ module Store
   end
   
   def Store::delete_from_remote_store(file_path)
-    Log.info "Deleting #{f  ile_path} from remote store..."
+    Log.info "Deleting #{file_path} from remote store..."
     bucket = Store::get_remote_store
-    files = bucket.objects.with_prefix remote_folder
+    files = bucket.objects.with_prefix file_path
     files.each { |file_obj| file_obj.delete }  
   end
   
