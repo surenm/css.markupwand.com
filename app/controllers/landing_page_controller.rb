480e1d507c8ffd54e243be873b0d823e3273b2cb
Rajagopal Natarajan <raj@goyaka.com>
2012-07-06 13:39:12 -0700

    Handling the case when users land up on getinvite page for some reason

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -8,11 +8,13 @@ class LandingPageController < ApplicationController
   
   def getinvite
     @email = params[:email]
+    unless @email.nil?
     users = User.where(:email => @email)
-    if(users.size==0)
-      name = @email.split("@")[0]
-      user = User.create!({:email=>@email, :name=>name})
-      user.save
+      if(users.size==0)
+        name = @email.split("@")[0]
+        user = User.create!({:email=>@email, :name=>name})
+        user.save
+      end
     end
   end
 end
