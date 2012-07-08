7e7278b4c41e1e846b77f5e8a7a3b055e8039d00
Alagu <allagappan@gmail.com>
2012-07-08 13:56:37 -0700

    Design globals file.

---
+++ app/models/design_globals.rb
@@ -0,0 +1,27 @@
+class DesignGlobals
+  # Having design mongo object as reference wont work
+  # because, belongs_to references do a Design.find design_id
+  # hence it returns a design object with new address space
+  # everytime. It means you have to save it. 
+  #
+  # Having a singleton/global helps because you don't have to 
+  # keep writing to mongodb.
+  attr_accessor :css_properties_inverted
+  @@designglobals = nil
+
+  def self.instance
+    if @@designglobals.nil?
+      @@designglobals = DesignGlobals.new
+    end
+
+    @@designglobals
+  end
+
+  def initialize
+    @css_properties_inverted = {}
+  end
+
+  def self.destroy
+    @@designglobals = nils
+  end
+end
