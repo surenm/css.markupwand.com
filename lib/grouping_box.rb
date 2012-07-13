b5f65c823be94194b9a08f889c88fbb140da6838
suren <suren@goyaka.com>
2012-07-14 00:25:07 +0530

    Ability to set an alternate bounds to grouping box

--- lib/grouping_box.rb
+++ lib/grouping_box.rb
@@ -4,6 +4,7 @@ class GroupingBox
   # orientation could be :normal, :left, :right
   def initialize(orientation)
     @orientation = orientation
+    @bounding_box = nil
     @children = []
   end
   
@@ -16,14 +17,23 @@ class GroupingBox
   end
   
   def bounds
-    bounding_boxes = []
-    @children.each do |child| 
-      if child.kind_of? BoundingBox
-        bounding_boxes.push child
-      elsif child.kind_of? GroupingBox
-        bounding_boxes.push child.bounds
+    if @bounding_box.nil?
+      bounding_boxes = []
+      @children.each do |child| 
+        if child.kind_of? BoundingBox
+          bounding_boxes.push child
+        elsif child.kind_of? GroupingBox
+          bounding_boxes.push child.bounds
+        end
       end
+      @bounding_box = BoundingBox.get_super_bounds bounding_boxes
     end
-    BoundingBox.get_super_bounds bounding_boxes
+    
+    return @bounding_box
   end
+  
+  def bounds=(bounding_box)
+    @bounding_box = bounding_box
+  end
+  
 end
