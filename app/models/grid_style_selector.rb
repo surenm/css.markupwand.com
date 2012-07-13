2a3a5516550631f1efe10cebc2efbf2625b142c5
suren <suren@goyaka.com>
2012-07-14 01:32:11 +0530

    Global margin logic

--- app/models/grid_style_selector.rb
+++ app/models/grid_style_selector.rb
@@ -53,14 +53,10 @@ class GridStyleSelector
         children_bounds     = self.grid.layers.collect { |layer| layer.bounds }
         children_superbound = BoundingBox.get_super_bounds children_bounds        
         margin_superbound   = BoundingBox.get_super_bounds margin_boxes
-          
+           
         if not margin_superbound.nil? and not children_superbound.nil?
-          if self.grid.offset_box_type == :grid_offset_box
-            margin[:top] = children_superbound.top - margin_superbound.top
-            margin[:left] = children_superbound.left - margin_superbound.left
-          elsif self.grid.offset_box_type == :row_offset_box
-            margin[:top] = children_superbound.top - margin_superbound.top
-          end
+          margin[:top] = children_superbound.top - margin_superbound.top
+          margin[:left] = children_superbound.left - margin_superbound.left
         end
       end
     end
