2605b67529e1c2febd395c4f9e02b868216e5ca1
suren <suren@goyaka.com>
2012-07-21 12:38:08 -0700

    Typo fix

--- config/initializers/00_constants.rb
+++ config/initializers/00_constants.rb
@@ -10,7 +10,7 @@ module Constants
   Constants::PROCESSING_QUEUE = "markupwand-processing"
   
   # Max retry with grouping
-  Constatns::GROUPING_MAX_RETRIES = 3
+  Constants::GROUPING_MAX_RETRIES = 3
   
   # Round to nearest 5
   def Constants::round_to_nearest_five(num)
