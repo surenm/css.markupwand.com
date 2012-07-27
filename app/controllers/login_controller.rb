b5f8b7f92a5cc550184a498877648fa3160718bd
Rajagopal Natarajan <raj@goyaka.com>
2012-07-27 10:24:26 -0700

    Redirecting already logged in and approved users to designs page

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,5 +1,9 @@
 class LoginController < ApplicationController
   def index
+    require_login
+    if @user != nil
+      redirect_to '/designs'
+    end
     @signup_enabled = !Constants::invite_gated?
   end
 
