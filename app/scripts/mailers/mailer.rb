80f1d9a8435966694d0f52d4970594233581a2ef
Alagu <allagappan@gmail.com>
2012-07-31 17:24:10 -0700

    Make the function static

--- app/scripts/mailers/mailer.rb
+++ app/scripts/mailers/mailer.rb
@@ -3,23 +3,24 @@ require "multimap"
 require "rest-client"
 require "pp"
 
-def mailgun_send 
-  data = Multimap.new
-  data[:from] = "Raj <raj@markupwand.com>"
-  data[:'reply-to'] = "Hackers <hackers@markupwand.com>"
-# Replace this for testing with required id and DO NOT commit
-  data[:to] =  "test@markupwand.mailgun.org"
-  data[:'h:Reply-To'] = "Hackers <hackers@markupwand.com>"
-  data[:subject] = "Status of your upload at Markupwand"
-  data[:text]  = File.open('failed.txt').read
-  puts "Sending to test@markupwand.mailgun.org"
-  begin
-   response = RestClient.post "https://api:key-3k01b6wme8-hzvhyowno9r0gccep7e17"\
-   "@api.mailgun.net/v2/markupwand.mailgun.org/messages", data
-   puts response.body
-  rescue Exception => e
-   puts e.inspect
+class Mailer
+  # Template would be absolute path of the .txt file /Users/alagu/Code/Goyaka/web/<somemore>/campaign.txt
+  def self.mailgun_send(template)
+    data = Multimap.new
+    data[:from] = "Raj <raj@markupwand.com>"
+    data[:'reply-to'] = "Hackers <hackers@markupwand.com>"
+  # Replace this for testing with required id and DO NOT commit
+    data[:to] =  "test@markupwand.mailgun.org"
+    data[:'h:Reply-To'] = "Hackers <hackers@markupwand.com>"
+    data[:subject] = "Status of your upload at Markupwand"
+    data[:text]  = File.open(template).read
+    puts "Sending to test@markupwand.mailgun.org"
+    begin
+     response = RestClient.post "https://api:key-3k01b6wme8-hzvhyowno9r0gccep7e17"\
+     "@api.mailgun.net/v2/markupwand.mailgun.org/messages", data
+     puts response.body
+    rescue Exception => e
+     puts e.inspect
+    end
   end
 end
-
-mailgun_send
