c90b52924cfc7703955c1777e21615f94faf3a10
Alagu <allagappan@gmail.com>
2012-08-02 07:46:32 -0700

    Search by multiple design ids

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,10 +6,17 @@ class AdminController < ApplicationController
     end_date = params.fetch "end_date", Time.now
     page = params.fetch "page", 0
 
-    status = params.fetch "status", nil 
+    status     = params.fetch "status", nil 
     user_email = params.fetch "user", nil
-    design = params.fetch "design", ""
-    design_id = design.split("-").last
+    designs    = params.fetch "designs", ""
+    design_splits = designs.split(",")
+    design_ids    = []
+    design_splits.each do |design_item|
+      design_id = design_item.strip.split('-').last
+      if not design_id.nil? and not design_id.empty?
+        design_ids.push design_id
+      end
+    end
 
     @query_args = {:created_at.gt => start_date, :created_at.lt => end_date}
     @query_args[:status] = status.to_sym if not status.nil? and not status == "all"
@@ -19,8 +26,8 @@ class AdminController < ApplicationController
     @query_args = {:created_at.gt => start_date, :created_at.lt => end_date}
     @query_args[:status] = status.to_sym if not status.nil? and not status == "all"
     
-    if not design_id.nil? and design_id != ""
-      @designs = Design.where(:_id => design_id).page(page)
+    if not design_ids.empty? and not design_ids.length == 0
+      @designs = Design.where(:_id.in => design_ids).page(page)
       all_designs = @designs
     else
       all_designs = Design.all
@@ -36,7 +43,7 @@ class AdminController < ApplicationController
     @results_data[:total_count] = all_designs.count
     @results_data[:status] = status if not status.nil?
     @results_data[:user] = user_email if not user_email.nil?
-    @results_data[:design] = design if not design.empty?
+    @results_data[:design] = designs if not designs.empty?
   end
   
   def reprocess
