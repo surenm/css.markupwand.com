b29e7fe613023db5e64e6e72ea93b1fca9fcdb30
suren <suren@goyaka.com>
2012-08-10 00:47:10 -0700

    Make screenshot work out of rails box itself

--- Procfile
+++ Procfile
@@ -1,6 +1,7 @@
 web:           bundle exec unicorn -p $PORT -c ./config/unicorn.rb
-worker:        bundle exec rake resque:work QUEUE=uploader,parser,pre_processor,generator,html_writer,screenshot
+worker:        bundle exec rake resque:work QUEUE=uploader,screenshot,parser,pre_processor,generator,html_writer,screenshot
 uploader:      bundle exec rake resque:work QUEUE=uploader
+screenshot:    bundle exec rake resque:work QUEUE=screenshot
 parser:        bundle exec rake resque:work QUEUE=parser
 generator:     bundle exec rake resque:work QUEUE=generator
 html_writer:   bundle exec rake resque:work QUEUE=html_writer
--- app/tasks/screenshot_job.rb
+++ app/tasks/screenshot_job.rb
@@ -1,22 +1,31 @@
 class ScreenshotJob
   @queue = :screenshot
   
-  def self.perform(design_item)
-    design_id = design_item.strip.split('-').last
+  def self.perform(design_id)
     design = Design.find design_id
     
     Log.info "Generating screenshots and thumbnails for #{design.name}..."
     
     design_folder  = Store.fetch_from_store design.store_key_prefix
-    photoshop_file = design.psd_file_path
+    photoshop_file = Rails.root.join 'tmp', 'store', design.psd_file_path
     psd_file_root  = File.basename design.psd_file_path, '.psd'
     
-    screenshot_file  = Rails.root.join 'tmp', 'store', design.store_psdjsprocessed_key, "output.png"
-    fixed_width_file = Rails.root.join 'tmp', 'store', design.store_key_prefix, "#{psd_file_root}-fixed.png"
-    thumbnail_file   = Rails.root.join 'tmp', 'store', design.store_key_prefix, "#{psd_file_root}-thumbnail.png"
+    psdjsprocessed_directory = Rails.root.join 'tmp', 'store', design.store_psdjsprocessed_key
+    FileUtils.mkdir psdjsprocessed_directory if not Dir.exists? psdjsprocessed_directory
     
+    screenshot_file  = Rails.root.join 'tmp', 'store', design.store_psdjsprocessed_key, "#{psd_file_root}.png"
+    fixed_width_file = Rails.root.join 'tmp', 'store', design.store_psdjsprocessed_key, "#{psd_file_root}-fixed.png"
+    thumbnail_file   = Rails.root.join 'tmp', 'store', design.store_psdjsprocessed_key, "#{psd_file_root}-thumbnail.png"
+    
+    screenshot_script = Rails.root.join 'tmp', 'psdjs', 'screenshot.coffee'
+    coffee_script = Rails.root.join 'tmp', 'psdjs', 'node_modules', '.bin', 'coffee'
+    
+    screenshot_cmd  = "#{coffee_script} #{screenshot_script} #{photoshop_file} #{screenshot_file}"
     fixed_width_cmd = "convert #{screenshot_file} -thumbnail '600x480>' -unsharp 0x.8 #{fixed_width_file}"
     thumbnail_cmd   = "convert #{screenshot_file} -thumbnail '180x240>' -unsharp 0x.8 #{thumbnail_file}"
+    
+    Log.info screenshot_cmd
+    system screenshot_cmd
     
     Log.info fixed_width_cmd
     system fixed_width_cmd
