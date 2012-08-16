29e3336a4e04bfd1a36beee5c6096ad1580c0036
Alagu <allagappan@gmail.com>
2012-08-15 22:47:31 -0700

    Return to dashboard after logging in.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -6,7 +6,7 @@ class AuthController < Devise::OmniauthCallbacksController
   end
   
   def after_sign_in_path_for(resource)
-    return request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    return dashboard_path
   end
 
   def admin
