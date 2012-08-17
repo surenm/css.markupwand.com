b50c713d25096c8a89f7224340e059a73601e178
suren <suren@goyaka.com>
2012-08-17 06:48:26 -0700

    Fixing asset generation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ TransformersWeb::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += ["app/*", "app/*"]
+  config.assets.precompile += ["app/*", "app/*", "lib/prettify.css", "lib/*.js"]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
