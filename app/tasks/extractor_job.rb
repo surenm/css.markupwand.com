96fa527e1384007779c8f40a42baafb477085cfb
suren <suren@goyaka.com>
2012-08-27 16:06:22 -0700

    Extractor job to extract out of photoshop items

---
+++ app/tasks/extractor_job.rb
@@ -0,0 +1,57 @@
+class ExtractorJob
+  extend Resque::Plugins::History
+  
+  @queue = :extractor
+  
+  def self.perform(design_id)
+    design = Design.find design_id
+      
+    Log.info "Extracting design data from photoshop file #{design.name}..."
+    
+    design_folder  = Store.fetch_from_store design.store_key_prefix
+    photoshop_file = Rails.root.join 'tmp', 'store', design.psd_file_path
+    psd_file_root  = File.basename design.psd_file_path, '.psd'
+    
+    processed_folder = Rails.root.join 'tmp', 'store', design.store_extracted_key
+    assets_directory = Rails.root.join processed_folder, "assets"
+    FileUtils.mkdir_p assets_directory if not Dir.exists? assets_directory
+    
+    processed_file   = Rails.root.join processed_folder, "#{design.safe_name_prefix}.json"
+    screenshot_file  = Rails.root.join processed_folder, "#{design.safe_name_prefix}.png"
+    fixed_width_file = Rails.root.join processed_folder, "#{design.safe_name_prefix}-fixed.png"
+    thumbnail_file   = Rails.root.join processed_folder, "#{design.safe_name_prefix}-thumbnail.png"
+    
+    extractor_script = Rails.root.join 'tmp', 'psdjs', 'extract.coffee'
+    coffee_script = Rails.root.join 'tmp', 'psdjs', 'node_modules', '.bin', 'coffee'
+    
+    extractor_command = "#{coffee_script} #{extractor_script} #{photoshop_file} #{processed_folder} #{design.safe_name_prefix}"
+
+    Log.info extractor_command
+    system extractor_command
+    
+    if not File.exists? screenshot_file
+      design.add_tag Design::ERROR_SCREENSHOT_FAILED
+      design.save!
+      return
+    end
+
+    fixed_width_cmd = "convert #{screenshot_file} -thumbnail '600x480>' -unsharp 0x.8 #{fixed_width_file}"
+    thumbnail_cmd   = "convert #{screenshot_file} -thumbnail '180x240>' -unsharp 0x.8 #{thumbnail_file}"
+    
+    Log.info fixed_width_cmd
+    system fixed_width_cmd
+    
+    Log.info thumbnail_cmd
+    system thumbnail_cmd
+    
+    Store.save_to_store processed_file, File.join(design.store_extracted_key, "#{design.safe_name_prefix}.json")
+    Store.save_to_store screenshot_file, File.join(design.store_extracted_key, "#{design.safe_name_prefix}.png")
+    Store.save_to_store fixed_width_file, File.join(design.store_extracted_key, "#{design.safe_name_prefix}-fixed.png")
+    Store.save_to_store thumbnail_file, File.join(design.store_extracted_key, "#{design.safe_name_prefix}-thumbnail.png")
+    
+    design.pre_processed = true
+    design.save!
+  
+    Log.info "Sucessfully completed extracting from photoshop file #{design.name}."
+  end
+end
