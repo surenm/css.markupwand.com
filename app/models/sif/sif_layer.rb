d3bf3a50ce7ea53de2c867904497df79a7f9d8f8
suren <suren@goyaka.com>
2012-09-05 15:26:41 -0700

    Update sif_layer according to layer parser

--- app/models/sif/sif_layer.rb
+++ app/models/sif/sif_layer.rb
@@ -1,21 +1,20 @@
 class Sif::SifLayer
   def self.create(layer_json, design)
     layer            = Layer.new
-    layer.name       = layer_json[:name]
-    layer.layer_type = layer_json[:layerType] 
-    layer.kind       = layer_json[:layerType] # TODO: fix this
-    layer.uid        = layer_json[:layerId]
-    layer.zindex     = layer_json[:zindex]
     layer.design     = design
-
-    raw_bounds 	= layer_json[:bounds]
-    bounds 		= BoundingBox.new raw_bounds[:top],
-      raw_bounds[:left], raw_bounds[:bottom], raw_bounds[:right]
+    
+    layer.name       = layer_json[:name]
+    layer.layer_type = layer_json[:type] 
+    layer.kind       = layer_json[:type]
+    layer.uid        = layer_json[:uid]
+    layer.zindex     = layer_json[:zindex]
+    
+    raw_bounds = layer_json[:bounds]
+    bounds     = BoundingBox.new raw_bounds[:top], raw_bounds[:left], raw_bounds[:bottom], raw_bounds[:right]
     
     layer.initial_bounds = bounds
-    design_bounds 		 = BoundingBox.new 0, 0, layer.design.height, layer.design.width
-    
-    layer_bounds 		 = bounds.inner_crop(design_bounds)
+    design_bounds = BoundingBox.new 0, 0, layer.design.height, layer.design.width
+    layer_bounds  = bounds.inner_crop(design_bounds)
 
     if layer_bounds.nil?
       layer.invalid_layer = true
