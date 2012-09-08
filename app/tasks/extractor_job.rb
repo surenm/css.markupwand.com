d57afa7716d829672eb243dabf3ee3bec5366bee
Alagu <allagappan@gmail.com>
2012-09-08 13:36:25 -0700

    Glob all files in that directory

--- app/tasks/extractor_job.rb
+++ app/tasks/extractor_job.rb
@@ -70,7 +70,7 @@ class ExtractorJob
     Store.save_to_store fixed_width_file, File.join(design.store_extracted_key, "#{design.safe_name_prefix}-fixed.png")
     Store.save_to_store thumbnail_file, File.join(design.store_extracted_key, "#{design.safe_name_prefix}-thumbnail.png")
     
-    Dir["#{assets_directory}/*"].each do |asset_file_path|
+    Dir.glob("#{assets_directory}/**/*").each do |asset_file_path|
       asset_basename = File.basename asset_file_path
       asset_destination_path = File.join design.store_extracted_key, "assets", asset_basename
       Store.save_to_store(asset_file_path, asset_destination_path)
