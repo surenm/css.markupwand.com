f2c7d23ace7b8f310240c3470ac86dc9ad8d2004
Alagu <allagappan@gmail.com>
2012-09-11 19:12:23 -0700

    Make Compassify to return array. Works on case-by-case basis.

--- lib/compassify.rb
+++ lib/compassify.rb
@@ -1,17 +1,17 @@
 module Compassify
   class << self
-    def get_scss(style_object)
-      css_rules = []
-      style_object.each do |key, object|
-        if Compassify.respond_to? key
-          style_string = (Compassify.send key, object)
-          css_rules.push style_string if (not style_string.nil?) and (not style_string.empty?)
+    def get_scss(key, style_object)
+      if Compassify.respond_to? key
+        style_string = (Compassify.send key, style_object)
+        
+        if (not style_string.nil?) and (not style_string.empty?)
+          return [style_string]
         else
-          css_rules.push "#{key} : #{object}"
-        end
+          return nil 
+        end 
+      else
+        ["#{key} : #{style_object}"]
       end
-
-      css_rules
     end
 
     # http://compass-style.org/examples/compass/css3/box_shadow/
@@ -21,17 +21,17 @@ module Compassify
         shadow_css_items.push "#{shadow[:color]} #{shadow[:vertical_offset]} #{shadow[:horizontal_offset]} #{shadow[:blur]} #{shadow[:spread]} #{shadow[:type]}"
       end
       
-      "@include box-shadow(" + shadow_css_items.join(', ') + ")"
+      ["@include box-shadow(" + shadow_css_items.join(', ') + ")"]
     end    
     
     def solid_fill(object)
-      "background-color: #{object}"
+      ["background-color: #{object}"]
     end
     alias :solid_color :solid_fill
 
     def gradient_fill(object)
       if object[:type] == 'linear'
-        "@include background-image(linear-gradient(#{object[:angle]}deg, #{object[:color_stops].join ', '}))"
+        ["@include background-image(linear-gradient(#{object[:angle]}deg, #{object[:color_stops].join ', '}))"]
       else
         nil
       end
@@ -39,16 +39,21 @@ module Compassify
     
     def border(object)
       if object.is_a? Hash
-        "border : #{object[:width]} solid #{object[:color]}"
+        ["border : #{object[:width]} solid #{object[:color]}"]
       else
-        "border : #{object}"
+        ["border : #{object}"]
       end
     end
 
     # This I think is still messy. Should fix it.
     def get_border_radius(radius)
-      "@include border-radius(#{radius})"
+      ["@include border-radius(#{radius})"]
     end
 
+    def text_gradient(gradient)
+      ["background: -webkit-linear-gradient(#{gradient[:angle]}deg, #{gradient[:color_stops].join ', '})",
+       "-webkit-background-clip: text",
+       "-webkit-text-fill-color: transparent"]
+    end
   end
 end
