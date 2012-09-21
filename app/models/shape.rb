56912d27a8fa8ec43efd78be84233ca5827d893b
suren <suren@goyaka.com>
2012-09-21 03:47:58 -0700

    Remove shape related ruby classes

--- app/models/shape.rb
+++
@@ -1,9 +0,0 @@
-module Shape
-  SUPPORTED_SHAPES = [Shape::Box, Shape::Circle]
-  def Shape.get_css_shape(path)
-    SUPPORTED_SHAPES.each do |supported_shape|
-      return supported_shape if supported_shape.my_type? path
-    end
-    return nil
-  end
-end
