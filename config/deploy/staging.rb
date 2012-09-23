c7fbd3f3696297cc534489981c57b29305e6fd93
suren <suren@goyaka.com>
2012-09-23 15:03:47 -0700

    Copy staging configs on deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -8,3 +8,11 @@ set :heroku_remote, "staging"
 
 # Servers list
 server "ec2-23-20-68-9.compute-1.amazonaws.com", :web, :app
+
+namespace :deploy do  
+  task :copy_staging_configs do 
+    run "cp #{shared_path}/staging_env #{current_path}/.env"
+  end
+end
+
+after 'deploy:create_symlink', 'deploy:copy_staging_configs'
