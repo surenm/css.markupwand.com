f45dc8e3a2b3d887ab9ac049ac0b78d7067ee8f1
suren <suren@goyaka.com>
2012-09-28 12:56:17 -0700

    Fix production capistrano after deploy task

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,3 +5,12 @@ set :deploy_to, "/opt/#{domain}"
 
 set :heroku_app, "markupwand"
 set :heroku_remote, "production"
+
+
+namespace :deploy do  
+  task :copy_prod_configs do 
+    run "cp #{shared_path}/prod_env #{current_path}/.env"
+  end
+end
+
+after 'deploy:create_symlink', 'deploy:copy_prod_configs'
