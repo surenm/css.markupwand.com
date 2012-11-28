84e409c5cc30c4fa064cda7f7f74d727e7a5f8ba
suren <suren@goyaka.com>
2012-11-28 14:19:39 +0530

    HTML job is broken

--- app/tasks/html_job.rb
+++ app/tasks/html_job.rb
@@ -1,12 +1,10 @@
-class GridJob
+class HtmlJob
   extend Resque::Plugins::History
 
   @queue = :worker
 
   def self.perform(design_id)
     design = Design.find design_id
-    design.create_grids
-
-    #Resque.enqueue HtmlJob, design_id
+    design.generate_markup
   end
 end
