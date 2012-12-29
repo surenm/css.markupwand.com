a57960c4a6be941712a424e8a9ebd8169b8146c1
suren <suren@goyaka.com>
2012-12-29 08:07:36 +0530

    Remove useless jobs

--- app/tasks/pre_processor_job.rb
+++
@@ -1,5 +0,0 @@
-class PreProcessorJob
-  @queue = :pre_processor
-  def self.perform(design_id)
-  end
-end
--- app/tasks/processor_job.rb
+++
@@ -1,8 +0,0 @@
-class ProcessorJob
-  @queue = :processor
-  
-  def self.perform(message)
-    # Dummy method so that queuing from web to this queue is possible. 
-    # Check transformers project for the same class
-  end
-end
