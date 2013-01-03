6a09383b0142900abc69b07fbc48c3be3f95716b
Alagu <allagappan@gmail.com>
2013-01-03 16:01:56 +0530

    Save regroup functions

--- app/controllers/design_controller.rb
+++ app/controllers/design_controller.rb
@@ -181,8 +181,8 @@ class DesignController < ApplicationController
         Store::rename_file File.join(@design.store_extracted_key, "assets", "images", original_name), File.join(@design.store_extracted_key, "assets", "images", final_name)
         Store::rename_file File.join(@design.store_generated_key, "assets", "images", original_name), File.join(@design.store_generated_key, "assets", "images", final_name)
         Store::rename_file File.join(@design.store_published_key, "assets", "images", original_name), File.join(@design.store_published_key, "assets", "images", final_name)
-        @design.save_sif!
-        @design.reparse
+        @design.sif.save!
+        @design.regroup
         render :json => {:status => "OK"} 
         return
       else
