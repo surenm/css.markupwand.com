21ae570059269ffeb9265851d8315be5507ab127
suren <suren@goyaka.com>
2013-01-21 14:56:01 +0530

    Typo fix on mongoid method

--- app/tasks/images_completed_job.rb
+++ app/tasks/images_completed_job.rb
@@ -3,7 +3,7 @@ class ImagesCompletedJob
   @queue = :worker
 
   def self.perform(design_id)
-    design = Design.fetch design_id
+    design = Design.find design_id
     design.photoshop_status = Design::STATUS_PROCESSING_DONE
     design.save!
   end
