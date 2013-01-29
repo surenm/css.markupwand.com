9a68a9f70a541eaa19b7a6bf6991546ed0632640
suren <suren@goyaka.com>
2013-01-29 15:10:21 +0530

    Just mention the css file!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ TransformersWeb::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += ["app/*", "app/*", "design.css.scss"]
+  config.assets.precompile += ["app/*", "app/*", "design.css"]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
