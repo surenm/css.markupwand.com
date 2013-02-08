c0c237bdc492209dd737ea9d3d1e356d36c3af57
Alagu <allagappan@gmail.com>
2013-02-08 18:01:18 +0530

    Exclude unused models.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,4 +1,6 @@
 RailsAdmin.config do |config|
+  config.excluded_models = ['DesignGallery', 'InviteRequest']
+
   config.authorize_with do
     redirect_to main_app.root_path unless warden.user.admin
   end
