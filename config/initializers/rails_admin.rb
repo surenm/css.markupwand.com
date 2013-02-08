e7e52673e134e1ef0713cdb66a8b792f4207f787
Alagu <allagappan@gmail.com>
2013-02-08 18:13:46 +0530

    Remove fonts also, they are not used.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,5 @@
 RailsAdmin.config do |config|
-  config.excluded_models = ['DesignGallery', 'InviteRequest']
+  config.excluded_models = ['DesignGallery', 'InviteRequest', 'SystemFont', 'UserFont']
 
   config.authorize_with do
     redirect_to main_app.root_path unless warden.user.admin
