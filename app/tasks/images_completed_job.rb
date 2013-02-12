83fe9ea1762ead5c69bc730105fc03e66a232be1
suren <suren@goyaka.com>
2013-02-12 16:54:37 +0530

    Don't need that check at all!

--- app/tasks/images_completed_job.rb
+++ app/tasks/images_completed_job.rb
@@ -7,10 +7,7 @@ class ImagesCompletedJob
     user = design.user
     src_folder = design.store_images_key
     destination_folder = File.join design.store_extracted_key, "images"
-    if Dir.exists? destination_folder
-      Store::copy_within_store_recursively src_folder, destination_folder
-    end
-
+    Store::copy_within_store_recursively src_folder, destination_folder
     design.photoshop_status = Design::STATUS_PROCESSING_DONE
     design.save!
     Resque.enqueue ChatNotifyJob, "#{user.name.to_s} (#{user.email.to_s})'s images are completed"
